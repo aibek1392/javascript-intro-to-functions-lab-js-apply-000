68f807ed244895cc03ee1bdd214b98484b05d90e
Aibek Ozhorov <ozhorov@gmail.com>
2019-06-03 19:37:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function greet (string){}
-function greet (string){return string}
-'HI grandma!'.toUppercase()//'Hi grandma'
-function whisper(string){}
-function whisper (string) {return string.toUpperCase()}
-'I love you grandma'.toUpperCase()//'I love you grandma'
+function shout(HiGrandma){
+  expect(shout('hello')).toEqual('HELLO')
+  return string.toUpperCase()
+  }
+'HI grandma!'.toUpperCase()//'Hi grandma'
+function whisper(string){
+  return string.toLowerCase()
+}
+'I love you Grandma'.toLowerCase()//'I love you Grandma'function whisper (string) {return string.toLowerCase()}
